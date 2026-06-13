b9b5918b58b90b857fab97e61eb37fedf7e9c480
ygftlp <ygftlp@163.com>
2026-06-13 12:30:55 +0800

    feat: implement Creator 3.8 project adapter

---
+++ source/adapters/creator-38x/project-adapter.ts
@@ -0,0 +1,17 @@
+import { ProjectAdapter, ProjectDescriptor } from '../contracts/project-adapter';
+
+export class Creator38xProjectAdapter implements ProjectAdapter {
+    describe(): ProjectDescriptor {
+        return {
+            name: Editor.Project.name,
+            path: Editor.Project.path,
+            uuid: Editor.Project.uuid,
+            version: (Editor.Project as any).version || '1.0.0',
+            creatorVersion: (Editor as any).versions?.creator || (Editor as any).versions?.cocos || 'Unknown'
+        };
+    }
+
+    queryConfig(name: string): Promise<any> {
+        return (Editor.Message.request as any)('project', 'query-config', name);
+    }
+}
